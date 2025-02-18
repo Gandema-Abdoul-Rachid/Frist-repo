82ea7762f863e241b55c5f1cc88f26f6f7f12b1b
Gandema-Abdoul-Rachid <rachidgandema5@gmail.com>
2025-02-18 16:46:15 +0000

    Ajout de la partie qui récupère et trie les chiffre du nombre saisie par l'utilisateur

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     des valeurs très grande
     */
     long int number,temp;
-    int i;
+    int taille,i,j; 
     //Boucle de recupération du nombre saisie par l'utilisateur
     do
     {
@@ -17,12 +17,41 @@ int main()
 
     temp =  number;     //Pour eviter la perte de la valeur saisie par l'utilisateur
     //Boucle de détermination du nombre de chiffre contenue dans "number"
-    for(i=0;temp;i++)
+    for(taille=0;temp;taille++)
     {
         temp /= 10;
     }
+    //printf ("le nombre de chiffre contenue dans %ld est : %d",number,i);
     
-    printf ("%d",i);
+    int tableau_chiffre[taille]; //Déclaration du tableau qui va contenir les chiffres de "number"
     
+    /*Cette boucle nous permet de separer les chiffre de "number" et de les stocker dans "tableau_chiffre"
+    */
+   temp = number;
+    for(i=0;i<taille;i++)
+    {
+        tableau_chiffre[i] = temp % 10;
+        temp /= 10;
+    }
+
+    //Trie du tableau en ordre croissant en utilisant l'algoritme de trie par sélection
+    for(i=0;i<taille;i++)
+    {
+        for(j=i+1;j<taille-1;j++)
+        {
+            if (tableau_chiffre[i]>tableau_chiffre[j])
+            {
+                temp = tableau_chiffre[i];
+                tableau_chiffre[i] = tableau_chiffre[j];
+                tableau_chiffre[j] = temp;
+            }
+        }
+    }
+    
+    printf("\nLes chiffre de contenu dans %ld dont : ",number);
+    for(i=0;i<taille;i++)
+    {
+        printf("%2d",tableau_chiffre[i]);
+    }
     return 0;
 }
